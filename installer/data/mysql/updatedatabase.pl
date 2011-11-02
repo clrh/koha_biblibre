a8dab3d79cde9ba61d782061a600f0664faf30ea
Jonathan Druart <jonathan.druart@biblibre.com>
2011-11-02 10:04:16 +0100

    MT8068: Add missing field borrower.lostcomment in kohastructure

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5308,6 +5308,15 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.06.00.073";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    $dbh->do(qq{
+        ALTER TABLE `borrowers` ADD `lostcomment` VARCHAR(255) DEFAULT NULL AFTER `lost`
+    });
+    print "Upgrade to $DBversion done (Add missing field borrower.lostcomment)\n";
+    SetVersion($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
