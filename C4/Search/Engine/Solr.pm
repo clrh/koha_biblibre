6d68980538e6d5dd4b34d53acdee0bc6d5b427eb
Jonathan Druart <jonathan.druart@biblibre.com>
2011-12-21 14:32:01 +0100

    [SOLR] Catch error "400 URL must be absolute"
    This error appears when syspref SolrAPI is not set correctly

--- C4/Search/Engine/Solr.pm
+++ C4/Search/Engine/Solr.pm
@@ -389,11 +389,14 @@ sub SimpleSearch {
         warn $err;
 
         $err =~ s#^[^\n]*\n##; # Delete first line
-        if ( not $err =~ 'Connection refused' ) {
+        if ( $err =~ "400 URL must be absolute" ) {
+            $err = "Your system preference 'SolrAPI' is not set correctly";
+        }
+        elsif ( not $err =~ 'Connection refused' ) {
             my $document = XMLin( $err );
             $err = "$$document{body}{h2} : $$document{body}{pre}";
         }
-       $$result{error} = $err;
+        $$result{error} = $err;
     }
 
     return $result; # if (ref($result) eq "Data::SearchEngine::Solr::Results");
